0a27431dc86b6e79649a24827f965e60f030ea03
lmsutools <lcomunicamos3@gmail.com>
2024-03-25 16:20:30 -0500

    launch.sh mp3 and wav tts scripts support- new argument

--- launch.sh
+++ launch.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-# Default to 1 worker if no argument is provided
+# Default to 1 worker if no argument is provided for the number of workers
 NUM_WORKERS=${1:-1}
+# Default to mp3 if no argument is provided for the format
+FORMAT=${2:-mp3}
 
+# Download the model first
 python modeldownload.py
-uvicorn tts_server:app --host 0.0.0.0 --port 6006 --workers $NUM_WORKERS --proxy-headers &
+
+# Check the format and start the appropriate server
+if [ "$FORMAT" = "wav" ]; then
+    echo "Launching WAV server"
+    uvicorn tts_server_wav:app --host 0.0.0.0 --port 6006 --workers $NUM_WORKERS --proxy-headers &
+else
+    echo "Launching MP3 server"
+    uvicorn tts_server_mp3:app --host 0.0.0.0 --port 6006 --workers $NUM_WORKERS --proxy-headers &
+fi
+
+# Wait for the server to start
 sleep 5
+
+# Run additional script
 python script.py
